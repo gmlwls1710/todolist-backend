6e9ded97f9c18956088515a0306978be51493a66
Heejin Ahn <gmlwls-12@hanmail.net>
2026-02-27 13:23:09 +0900

    Allow Authorization header in CORS

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
